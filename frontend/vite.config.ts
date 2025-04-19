849c7e6e6f8a22a96d0ddef41db0850e0ad923df
Chirag Sharma <chiragnoida3345@gmail.com>
2025-04-19 22:48:12 +0530

    Correct asset loading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,14 @@ export default defineConfig({
 		outDir: "dist",
 		assetsDir: "assets",
 		emptyOutDir: true,
-		sourcemap: true
-	}
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ['react', 'react-dom', 'react-router-dom'],
+				},
+			},
+		},
+	},
+	base: '/',
 });
